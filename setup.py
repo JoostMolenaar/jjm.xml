07ed0edb963ac35c42963a5b439098ac1199e215
Joost Molenaar <j.j.molenaar@gmail.com>
2012-11-12 20:24:06 +0100

    Extracted xml.py for generating XML

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import distutils.core
+
+distutils.core.setup(
+    name='xml',
+    version='0.1',
+    description='Functions for generating XML',
+    author='Joost Molenaar',
+    author_email='j.j.molenaar@gmail.com',
+    url='http://github.com/JoostMolenaar/xml',
+    packages=['jjm.xml'])
