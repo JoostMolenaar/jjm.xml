bfba9b25dd4b256ae3cdf7b34cc92facce77260e
Joost Molenaar <j.j.molenaar@gmail.com>
2014-01-08 10:28:59 +0100

    Cache results of setup.py

---
+++ Makefile
@@ -0,0 +1,10 @@
+NAME = xmlist
+
+PIP_NAME = xmlist
+PIP_REQ = requirements.txt
+
+MAIN ?= 
+
+PKG = 
+
+include build/Makefile
--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
-import distutils.core
+repo_names = []
+package_names = []
+static_dirs = []
 
-distutils.core.setup(
-    name='jjm.xml',
-    version='0.1',
-    description='Functions for generating XML',
-    author='Joost Molenaar',
-    author_email='j.j.molenaar@gmail.com',
-    url='http://github.com/JoostMolenaar/xml',
-    packages=['jjm.xml'])
+import sys
+if len(sys.argv) == 2 and sys.argv[1] == '--install-requires':
+    print '\n'.join(package_names + repo_names)
+elif len(sys.argv) == 2 and sys.argv[1] == '--repo-names':
+    print '\n'.join(repo_names)
+elif len(sys.argv) == 2 and sys.argv[1] == '--package-names':
+    print '\n'.join(package_names)
+elif len(sys.argv) == 2 and sys.argv[1] == '--static-dirs':
+    print '\n'.join(static_dirs)
+else:
+    import os
+    import setuptools
+    setuptools.setup(
+        name='xmlist',
+        version='0.1',
+        description='Functions for generating XML',
+        author='Joost Molenaar',
+        author_email='j.j.molenaar@gmail.com',
+        url='http://github.com/j0057/xmlist',
+        py_modules=['xmlist'])
+        data_files=[ (root, map(lambda f: root + '/' + f, files))
+                     for src_dir in static_dirs
+                     for (root, dirs, files) in os.walk(src_dir) ],
+        install_requires=package_names + repo_names
+    )
+
+
