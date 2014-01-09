a5d74d0f55074dd095decdb3a260e3f2f496b589
Joost Molenaar <j.j.molenaar@gmail.com>
2014-01-09 19:32:34 +0100

    Update setup.py to make use of setuptools-metadata

--- setup.py
+++ setup.py
@@ -1,33 +1,28 @@
 #!/usr/bin/env python2.7
 
 repo_names = []
-package_names = []
+dist_names = []
 static_dirs = []
 
-import sys
-if len(sys.argv) == 2 and sys.argv[1] == '--install-requires':
-    print '\n'.join(package_names + repo_names)
-elif len(sys.argv) == 2 and sys.argv[1] == '--repo-names':
-    print '\n'.join(repo_names)
-elif len(sys.argv) == 2 and sys.argv[1] == '--package-names':
-    print '\n'.join(package_names)
-elif len(sys.argv) == 2 and sys.argv[1] == '--static-dirs':
-    print '\n'.join(static_dirs)
-else:
-    import os
-    import setuptools
-    setuptools.setup(
-        name='xmlist',
-        version='0.1',
-        description='Functions for generating XML',
-        author='Joost Molenaar',
-        author_email='j.j.molenaar@gmail.com',
-        url='http://github.com/j0057/xmlist',
-        py_modules=['xmlist'],
-        data_files=[ (root, map(lambda f: root + '/' + f, files))
-                     for src_dir in static_dirs
-                     for (root, dirs, files) in os.walk(src_dir) ],
-        install_requires=package_names + repo_names
-    )
+import os
+from setuptools import setup
+
+setup(
+    author='Joost Molenaar',
+    author_email='j.j.molenaar@gmail.com',
+    url='http://github.com/j0057/xmlist',
+    description='Functions for generating XML',
+    name='xmlist',
+    version='0.1',
+    py_modules=['xmlist'],
+    data_files=[ (root, map(lambda f: root + '/' + f, files))
+                 for src_dir in static_dirs
+                 for (root, dirs, files) in os.walk(src_dir) ],
+    install_requires=package_names+repo_names,
+    custom_metadata={
+        'x_repo_names': repo_names,
+        'x_dist_names': dist_names,
+        'x_static_dirs': static_dirs
+    })
 
 
